a5b7fb75a3c1770af5a5ef16368294aa73fc49ff
MishioNathaniel <nathanielmishio@yahoo.com>
2020-05-30 17:36:07 +0000

    added files

---
+++ kuditweetvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\`mishio alhassan\onedrive\desktop\djangoapp\kuditweetweet\kuditweetvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
